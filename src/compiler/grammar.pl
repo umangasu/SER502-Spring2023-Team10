a7d4efa547fb647701ba559172fb480c68002677
Sanket Kapse <sanket8397@gmail.com>
2023-04-15 23:44:04 -0700

    grammar added for basic elements

--- src/compiler/grammar.pl
+++ src/compiler/grammar.pl
@@ -1,5 +1,7 @@
 :- table block/2.
 
+
+
 program --> [program], ['{'], block , ['}'].
 
 % <block> ::= <statement> | <statement> <block>
@@ -8,3 +10,9 @@ block --> block, statement.
 
 
 statement --> [].
+
+identifier --> [I], {atom(I), \+ member(I, [program, for, if, else, for, while, range, print, int, float, char, string, bool, in])}.
+string --> [S], {atom(S)}.
+integer --> [N], {integer(N)}.
+float --> [F], {float(F)}.
+boolean --> [true] | [false].
