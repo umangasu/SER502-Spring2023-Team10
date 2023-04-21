7d8a4df72a1002b8e7003bbd3c25750c47e8db92
sohampatil <sohampatil.2303@gmail.com>
2023-04-21 02:01:30 -0700

    Added grammar for variables

--- src/compiler/grammar.pl
+++ src/compiler/grammar.pl
@@ -1,4 +1,7 @@
 :- table block/2.
+:- table expression/2.
+:- table term/2.
+:- table factor/2.
 
 
 
@@ -8,11 +11,31 @@ program --> [program], ['{'], block , ['}'].
 block --> statement.
 block --> block, statement.
 
+% <statement> ::= <declaration> ";" | <assignment> ";" | <if_statement> | 
+% <while_loop> | <for_loop> | <for_range> | <print_statement> ";"
+statement --> declaration, [;].
+statement --> assignment [;].
+statement --> if_statement.
+statement --> while_loop.
+statement --> for_loop.
+statement --> for_range.
+statement --> print_statement, [;].
 
-statement --> [].
+% <declaration> ::= <type> <variable> | <type> <variable> "," <variable1>
+declaration --> type, variable.
+declaration --> type, variable, [,], variable1.
 
-identifier --> [I], {atom(I), \+ member(I, [program, for, if, else, for, while, range, print, int, float, char, string, bool, in])}.
-string --> [S], {atom(S)}.
-integer --> [N], {integer(N)}.
-float --> [F], {float(F)}.
-boolean --> [true] | [false].
+% <type> ::= "int" | "float" | "char" | "string" | "bool"
+type --> [int].
+type --> [float].
+type --> [char].
+type --> [string].
+type --> [bool].
+
+% <variable> ::= <identifier> | <assignment>
+variable --> identifier.
+variable --> assignment.
+
+% <variable1> ::= <variable> "," <variable1> | <variable>
+variable1 --> variable, ",", variable1.
+variable1 --> variable.
