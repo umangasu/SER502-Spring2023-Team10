d12b095fe699c3cc76e33814a87512f7c3a23d11
Umang Sahastransu <usahastr@asu.edu>
2023-04-22 22:57:34 -0700

    Lexer or tokenizer created

---
+++ src/tokenize/lexer.py
@@ -0,0 +1,70 @@
+import os.path
+import re
+
+# Define the regular expressions for each token
+tokens = [
+    ('INT', r'\bint\b'),
+    ('IF', r'\bif\b'),
+    ('ELSE', r'\belse\b'),
+    ('WHILE', r'\bwhile\b'),
+    ('FOR', r'\bfor\b'),
+    ('IN', r'\bin\b'),
+    ('RANGE', r'\brange\b'),
+    ('PRINT', r'\bprint\b'),
+    ('ASSIGN', r'='),
+    ('EQ', r'=='),
+    ('LE', r'<='),
+    ('QUESTION', r'\?'),
+    ('COLON', r':'),
+    ('SEMICOLON', r';'),
+    ('LBRACE', r'{'),
+    ('RBRACE', r'}'),
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
+    ('ID', r'[a-zA-Z_]\w*'),
+    ('NUM', r'\d+'),
+    ('PLUS', r'\+'),
+    ('MINUS', r'-'),
+    ('MUL', r'\*'),
+    ('DIV', r'/'),
+    ('MOD', r'%'),
+    ('COMMA', r','),
+    ('FORLOOP', r'for\s*\('),
+    ('RANGELOOP', r'range\s*\('),
+    ('WHITESPACE', r'\s+')
+]
+
+tokenized_output = []
+
+
+# Define the function to tokenize the input string
+def tokenize(filename):
+    with open(os.path.dirname(__file__) + '/../../data/' + filename + '.eva') as file:
+        program = file.read()
+    text_file = open("sample.txt", "w")
+    # Combine the regular expressions into a single pattern
+    pattern = '|'.join('(?P<%s>%s)' % pair for pair in tokens)
+    # Tokenize the input string
+    for match in re.finditer(pattern, program):
+        token_type = match.lastgroup
+        token_value = match.group()
+        # Ignore whitespace tokens
+        if token_type == 'WHITESPACE':
+            continue
+        tokenized_output.append('\'' + token_value + '\'\n')
+    text_file.write(''.join(tokenized_output))
+
+    return tokenized_output
+
+
+# print('Tokens for program 1 : ', ''.join(tokenize("program1")))
+# for token in tokenize("program1"):
+#     print(token)
+#
+# print('Tokens for program 2 :')
+# for token in tokenize("program2"):
+#     print(token)
+#
+# print('Tokens for program 3 :')
+# for token in tokenize("program3"):
+#     print(token)
--- src/tokenize/lexical.py
+++ src/tokenize/lexical.py
@@ -1,4 +1,5 @@
 import os.path
+
 with open(os.path.dirname(__file__) + '/../../data/dummy.eva') as file:
     code = file.read()
 
@@ -6,22 +7,25 @@ lines = code.split('\n')
 
 lines = [line.strip() for line in lines]
 
-
 tokens = []
 for line in lines:
     line_tokens = line.split()
     for token in line_tokens:
         if token[-1] == ';':
-            tokens.append(token[:len(token)-1])
+            tokens.append(token[:len(token) - 1])
             tokens.append(',')
             tokens.append(';')
             tokens.append(',')
         else:
-            tokens.append(token) 
-            tokens.append(',')               
+            tokens.append(token)
+            tokens.append(',')
 
-
-output = tokens[:len(tokens)-1]
+output = tokens[:len(tokens) - 1]
+tokenized_output = ''.join(output)
+final_tokenized_output = []
+for token in tokens:
+    final_tokenized_output.append('\'' + token + '\'')
 
 print(output)
-print(''.join(output))
+print("Tokenized output : ", tokenized_output)
+print("Final output : ", ''.join(final_tokenized_output))
