8251add4717a15e6e086aa6e04076839e903806c
Umang Sahastransu <usahastr@asu.edu>
2023-04-27 00:21:49 -0700

    Added tokenization of double quotes

--- src/tokenize/lexer.py
+++ src/tokenize/lexer.py
@@ -62,15 +62,30 @@ def tokenize(filename):
         # Ignore whitespace tokens
         if token_type == 'WHITESPACE':
             continue
+        elif token_type == 'PRINT_TEXT':
+            print("Print text : ", token_value)
+            if len(token_value) > 0:
+                if token_value[0] == '"' and token_value[len(token_value) - 1] == '"':
+                    tokenized_output.append('\'\"\'\n')
+                    tokenized_output.append('\'' + token_value[1:len(token_value) - 1] + '\'\n')
+                    tokenized_output.append('\'\"\'\n')
         elif token_type == 'PRINTEXP':
             print_value = token_value[6:len(token_value) - 1].split(',')
             print('print value : ', print_value)
             tokenized_output.append('\'' + "print" + '\'\n' + '\'' + "(" + '\'\n')
             for value in print_value:
-                tokenized_output.append('\'' + value.strip() + '\'\n')
+                raw_value = value.strip()
+                if raw_value[0] == '"' and raw_value[len(raw_value) - 1] == '"':
+                    tokenized_output.append('\'\"\'\n')
+                    tokenized_output.append('\'' + raw_value[1:len(raw_value) - 1] + '\'\n')
+                    tokenized_output.append('\'\"\'\n')
+                else:
+                    raw_value = '\'' + raw_value + '\'\n'
+                    tokenized_output.append(raw_value)
             tokenized_output.append('\'' + ")" + '\'\n')
             continue
-        tokenized_output.append('\'' + token_value + '\'\n')
+        else:
+            tokenized_output.append('\'' + token_value + '\'\n')
     tokenized_output.pop()
     text_file.write(''.join(tokenized_output))
 
